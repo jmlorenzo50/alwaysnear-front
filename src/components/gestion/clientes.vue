7240a7e035c7d4a235d235227254c7bd3c4e6491
jorge <c0de42.com@gmail.com>
2018-12-26 23:22:52 +0100

    Maqueta. Se añaden los clientes.

--- src/components/gestion/clientes.vue
+++ src/components/gestion/clientes.vue
@@ -1,3 +1,131 @@
 <template>
-  <h1>Clientes</h1>  
+  <div>
+      <b-container fluid>
+          <div class="botoneraL">
+            <b-form-select required v-model="filtropais" :options="paises" class="mb-3" />
+          </div>
+          <div class="botoneraL">
+           <button  type="button" class="btn btn-primary btn-xs">Buscar</button>
+          </div>
+          <div class="botonera">
+            <nuevo-cliente></nuevo-cliente>
+          </div>
+      </b-container>
+
+    <div class="panel panel-default">
+        <b-table show-empty
+                 stacked="md"
+                 :items="items"
+                 :fields="fields"
+                 :current-page="currentPage"
+                 :per-page="perPage"
+                 :filter="filter"
+                 :sort-by.sync="sortBy"
+                 :sort-desc.sync="sortDesc"
+                 :sort-direction="sortDirection"
+        >
+            <template slot="actions" slot-scope="row">
+              <button                                type="button" class="btn btn-secondary btn-xs btnEdit"></button>
+              <button v-if="row.item.fcbaja != null" type="button" class="btn btn-secondary btn-xs btnPlus"></button>
+              <button v-if="row.item.fcbaja == null" type="button" class="btn btn-danger btn-xs btnRemove"></button>
+            </template>
+        </b-table>
+
+        <div style="float: right;">
+            <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0 texo-azul" />
+        </div>
+    </div>
+  </div>
+
 </template>
+
+
+<script>
+
+import NuevoCliente from './clientesmodal.vue'
+
+const items = [
+  { nombre: 'cliente01', tipoCliente: 'distribuidor', pais: 'Georgia', contacto: 'Pabloski', email: 'email@gmail.com', direccion: 'España, Madrid, C/Pez 1', fcbaja: null },
+  { nombre: 'cliente02', tipoCliente: 'distribuidor', pais: 'Georgia', contacto: 'Pabloski', email: 'email@gmail.com', direccion: 'España, Madrid, C/Pez 1', fcbaja: '01-12-2019' },
+  { nombre: 'cliente03', tipoCliente: 'cliente', pais: 'Rusia', contacto: 'Iván', email: 'email@gmail.com', direccion: 'España, Madrid, C/Pez 1', fcbaja: '01-12-2019' },
+  { nombre: 'cliente04', tipoCliente: 'cliente', pais: 'Brasil', contacto: 'Ronaldo', email: 'email@gmail.com', direccion: 'España, Madrid, C/Pez 1', fcbaja: null }
+]
+
+export default {
+  components: {
+    NuevoCliente
+  },
+
+  data () {
+    return {
+      items: items,
+      fields: [
+             { key: 'nombre', label: 'Login del usuario', sortable: true, sortDirection: 'desc' },
+             { key: 'tipoCliente', label: 'Rol del usuario', sortable: true, sortDirection: 'desc' },
+             { key: 'pais', label: 'Rol del usuario', sortable: true, sortDirection: 'desc' },
+             { key: 'fcbaja', label: 'Fecha de baja' },
+             { key: 'actions', label: 'Acciones' }
+      ],
+      currentPage: 1,
+      perPage: 8,
+      totalRows: items.length,
+      pageOptions: [ 5, 10, 15 ],
+      sortBy: null,
+      sortDesc: false,
+      sortDirection: 'asc',
+      filter: null,
+      filtropais: null,
+      paises: [
+        { value: null, text: '-- seleccione un país --' },
+        { value: 'ES', text: 'España' },
+        { value: 'RUS', text: 'Rusia' },
+        { value: 'GEO', text: 'Georgia' }
+      ],
+      computed: {
+        sortOptions () {
+          // Create an options list from our fields
+          return this.fields
+          .filter(f => f.sortable)
+          .map(f => { return { text: f.label, value: f.key } })
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .botonera {
+    float: right;
+    padding-top: 1em;
+    padding-bottom: 1em;
+  }
+  .botoneraL {
+    float: left;
+    padding-top: 1em;
+    padding-left: 1em;
+    padding-bottom: 1em;
+  }
+  .btnEdit {
+    background-image: url('../../assets/png/edit.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 1.5em;
+    height: 1.5em;
+  }
+  .btnRemove {
+    background-image: url('../../assets/png/remove.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 1.5em;
+    height: 1.5em;
+  }
+  .btnPlus {
+    background-image: url('../../assets/png/plus.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 1.5em;
+    height: 1.5em;
+  }
+
+</style>
---
+++ src/components/gestion/clientesmodal.vue
@@ -0,0 +1,262 @@
+<template>
+  <div>
+    <b-button @click="mostrarModal()"
+              variant="outline-primary"
+              v-b-popover.hover="'Crea un nuevo cliente'"
+              title="Ayuda">Nuevo</b-button>
+    <b-modal ref="myModalRef"
+             size="lg"
+             title="Ficha de cliente"
+             header-bg-variant="primary"
+             header-text-variant="light"
+    >
+      <b-card no-body>
+      <b-tabs pills card>
+        <b-tab title="Datos personales" active>
+          <b-container class="text-left" fluid>
+             <b-row class="mb-1 text-center">
+               <b-col cols="4"><b>Campo</b></b-col>
+               <b-col><b>Valor</b></b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">Nombre</b-col>
+               <b-col>
+                 <b-form-input type="text"
+                               required
+                               v-model="cliente.nombre"
+                               placeholder="Escribe un nombre del cliente"/>
+               </b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">Documento identificación</b-col>
+               <b-col>
+                 <b-form-input type="text"
+                               required
+                               v-model="cliente.documento"
+                               placeholder="Escribe el documento de identificación"/>
+               </b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">País</b-col>
+               <b-col>
+                 <b-form-select required v-model="cliente.pais" :options="paises" class="mb-3" />
+               </b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">Tipo cliente</b-col>
+               <b-col>
+                 <b-form-select required v-model="cliente.tipo" :options="tipos" class="mb-3" />
+               </b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">Nombre Contacto</b-col>
+               <b-col>
+                 <b-form-input type="text"
+                               required
+                               v-model="cliente.contacto.nombre"
+                               placeholder="Escribe un nombre de contacto"/>
+               </b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">Teléfono Contacto</b-col>
+               <b-col>
+                 <b-form-input type="text"
+                               required
+                               v-model="cliente.contacto.telefono"
+                               placeholder="Escribe un teléfono"/>
+               </b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">Email Contacto</b-col>
+               <b-col>
+                 <b-form-input type="text"
+                               required
+                               v-model="cliente.contacto.email"
+                               placeholder="Escribe un correo electrónico"/>
+               </b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">Dirección de Contacto</b-col>
+               <b-col>
+                 <b-form-input type="text"
+                               required
+                               v-model="cliente.contacto.direccion"
+                               placeholder="Escribe la dirección de contacto"/>
+               </b-col>
+             </b-row>
+
+             <b-row class="mb-1">
+               <b-col cols="4">Ámbito</b-col>
+               <b-col>
+                 <b-form-select required v-model="cliente.ambito" :options="ambitos" class="mb-3" />
+               </b-col>
+             </b-row>
+
+             <b-row>
+               <b-col cols="4">Fecha baja</b-col>
+               <b-col><b-form-input type="date" v-model="fcbaja" /></b-col>
+             </b-row>
+           </b-container>
+        </b-tab>
+
+        <b-tab title="Licencias">
+
+          <div class="panel panel-default">
+              <b-table show-empty
+                       stacked="md"
+                       :items="items"
+                       :fields="fields"
+                       :current-page="currentPage"
+                       :per-page="perPage"
+                       :filter="filter"
+                       :sort-by.sync="sortBy"
+                       :sort-desc.sync="sortDesc"
+                       :sort-direction="sortDirection"
+              >
+
+                  <template slot="fcactivacion" slot-scope="row">
+                    <button v-if="row.item.fcactivacion == null && row.item.fcbaja == null" type="button" class="btn btn-secondary btn-xs">Activar</button>
+                    <p v-if="row.item.fcactivacion != null">{{row.item.fcactivacion}}</p>
+                  </template>
+
+                  <template slot="fcrenovacion" slot-scope="row">
+                    <button v-if="row.item.fcactivacion != null && row.item.fcrenovacion == null && row.item.fcbaja == null" type="button" class="btn btn-secondary btn-xs">Renovar</button>
+                    <p v-if="row.item.fcrenovacion != null">{{row.item.fcrenovacion}}</p>
+                  </template>
+
+                  <template slot="actions" slot-scope="row">
+                    <button v-if="row.item.fcbaja != null" type="button" class="btn btn-secondary btn-xs btnPlus"></button>
+                    <button v-if="row.item.fcbaja == null" type="button" class="btn btn-danger btn-xs btnRemove"></button>
+                  </template>
+              </b-table>
+
+              <div style="float: right;">
+                  <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0 texo-azul" />
+              </div>
+          </div>
+
+
+
+
+        </b-tab>
+      </b-tabs>
+      </b-card>
+
+
+
+      <div slot="modal-footer" class="w-100" style="text-align: center">
+         <b-btn size="sm" style="width: 100px; margin-left: 25px; margin-right: 25px;" variant="primary" @click="btnAceptar">
+           Aceptar
+         </b-btn>
+         <b-btn size="sm" style="width: 100px; margin-left: 25px; margin-right: 25px;" variant="outline-primary" @click="btnCancelar" >
+           Cancelar
+         </b-btn>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+
+const items = [
+  { licencia: 'AAABBB-001', fcactivacion: '01-01-2015', fcrenovacion: '01-01-2015', fcbaja: '01-01-2015' },
+  { licencia: 'AAABBB-002', fcactivacion: '01-01-2015', fcrenovacion: '01-01-2015', fcbaja: null },
+  { licencia: 'AAABBB-003', fcactivacion: '01-01-2015', fcrenovacion: null, fcbaja: null },
+  { licencia: 'AAABBB-004', fcactivacion: null, fcrenovacion: null, fcbaja: null }
+]
+
+export default {
+  data () {
+    return {
+      modalShow: false,
+      items: items,
+      fields: [
+             { key: 'licencia', label: 'Licencias' },
+             { key: 'fcactivacion', label: 'Activación' },
+             { key: 'fcrenovacion', label: 'Renovación' },
+             { key: 'actions', label: 'Acciones' }
+      ],
+
+      currentPage: 1,
+      perPage: 8,
+      totalRows: items.length,
+      pageOptions: [ 5, 10, 15 ],
+      sortBy: null,
+      sortDesc: false,
+      sortDirection: 'asc',
+      filter: null,
+
+      cliente: {
+        nombre: null,
+        documento: null,
+        pais: null,
+        tipo: null,
+        ambito: null,
+        contacto: {
+          nombre: null,
+          telefono: null,
+          email: null,
+          direccion: null
+        },
+        licencias: [
+          {codigo: null, fcactivacion: null, fcrenovacion: null, fcbaja: null},
+          {codigo: null, fcactivacion: null, fcrenovacion: null, fcbaja: null},
+          {codigo: null, fcactivacion: null, fcrenovacion: null, fcbaja: null}
+        ]
+      },
+      rol: null,
+      fcbaja: '',
+      tipos: [
+        { value: null, text: '-- seleccione un tipo --' },
+        { value: '1', text: 'Cliente' },
+        { value: '2', text: 'Cliente V.I.P.' },
+        { value: '3', text: 'Franquicia' }
+      ],
+      paises: [
+        { value: null, text: '-- seleccione un país --' },
+        { value: 'ES', text: 'España' },
+        { value: 'RUS', text: 'Rusia' },
+        { value: 'GEO', text: 'Georgia' }
+      ],
+      ambitos: [
+        { value: null, text: '-- seleccione un ámbito --' },
+        { value: 'ES', text: 'España' },
+        { value: 'RUS', text: 'Rusia' },
+        { value: 'GEO', text: 'Georgia' }
+      ]
+    }
+  },
+
+  methods: {
+    mostrarModal () {
+      console.log('mostrarModal ' + this.modalShow)
+
+      this.cliente.nombre = null
+      this.cliente.documento = null
+      this.cliente.pais = null
+      this.cliente.tipo = null
+      this.cliente.ambito = null
+      this.cliente.contacto.nombre = null
+      this.cliente.contacto.telefono = null
+      this.cliente.contacto.email = null
+      this.cliente.contacto.direccion = null
+      this.cliente.fcbaja = ''
+      this.$refs.myModalRef.show()
+    },
+    btnAceptar () {
+      this.$refs.myModalRef.hide()
+    },
+    btnCancelar () {
+      this.$refs.myModalRef.hide()
+    }
+  }
+}
+</script>
